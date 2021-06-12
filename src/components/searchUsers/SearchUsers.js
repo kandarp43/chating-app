9b483a1dd3b17736cc5424c0ccc16b107783a652
kandarp dangi <kandarpdangi@gmail.com>
2021-06-12 16:03:37 +0530

    init

--- src/components/searchUsers/SearchUsers.js
+++ src/components/searchUsers/SearchUsers.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './SearchUsers.scss'
+import SearchIcon from '@material-ui/icons/Search'
 
 function SearchUsers() {
   return (
     <div className='search'>
+      <SearchIcon className='icon-block' style={{ color: '#6d6d6d' }} />
       <input type='text' placeholder='search' />
     </div>
   )
