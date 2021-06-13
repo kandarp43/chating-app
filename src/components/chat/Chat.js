04a607c44f4a8e92f0a3b60c42dcdf6a3692c356
kandarp dangi <kandarpdangi@gmail.com>
2021-06-13 16:03:43 +0530

    chat-section

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import './Chat.scss'
 function Chat() {
-  return <div>Chat</div>
+  return (
+    <div className='chat-container'>
+      <div className='chat'>
+        <span className='name'>kandarp</span>
+        <p className='msg'>this is message</p>
+        <span className='time'>10:10</span>
+      </div>
+    </div>
+  )
 }
 
 export default Chat
