8ddf13a72b5fd4e359a1a15792dc355e34f3baa4
kandarp dangi <kandarpdangi@gmail.com>
2021-06-14 22:38:08 +0530

    init

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -8,7 +8,7 @@ function Chat() {
         <p className='msg'>this is message</p>
         <span className='time'>10:10</span>
       </div>
-      <div className='chat you'>
+      <div className='chat '>
         <span className='name'>kandarp</span>
         <p className='msg'>this is message</p>
         <span className='time'>10:10</span>
