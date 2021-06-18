106db3bc5675cd39584d04cebcaf931bfbfa01d9
kandarp dangi <kandarpdangi@gmail.com>
2021-06-18 18:29:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import SearchUsers from './components/searchUsers/SearchUsers'
 import ProfileSection from './components/profileSection/ProfileSection'
 import Chatsection from './components/chatSection/Chatsection'
 import './App.scss'
+import Login from './components/login/Login'
 
 function App() {
   return (
-    <div className='app'>
-      <div className='left-side'>
-        <ProfileSection />
-        <SearchUsers />
-        <ChatCards />
-        <ChatCards />
-        <ChatCards />
-        <ChatCards />
-      </div>
-      <div className='right-side'>
-        <Chatsection />
-      </div>
-    </div>
+    <>
+      {true ? (
+        <Login />
+      ) : (
+        <div className='app'>
+          <div className='left-side'>
+            <ProfileSection />
+            <SearchUsers />
+            <ChatCards />
+            <ChatCards />
+            <ChatCards />
+            <ChatCards />
+          </div>
+          <div className='right-side'>
+            <Chatsection />
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
